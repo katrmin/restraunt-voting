8145c6382f5fa02e6f549d7952b1b0ae8e65fc3f
katrmin <katrmin@yandex.ru>
2022-01-21 02:06:56 +0300

    hw1-Apply patch 2_01_data_jpa.patch

--- src/test/java/ru/javaops/bootjava/RestaurantVotingApplicationTests.java
+++ src/test/java/ru/javaops/bootjava/RestaurantVotingApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RestrauntVotingApplicationTests {
+class RestaurantVotingApplicationTests {
 
     @Test
     void contextLoads() {
